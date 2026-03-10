2c2466e60e5790eb7df950148e210414cbd3c591
arizbesilva0-star <arizbesilva0@gmail.com>
2026-03-10 09:58:49 -0600

    Add files via upload

---
+++ teoria/metodosVSMain/Cuenta.py
@@ -0,0 +1,27 @@
+"""
+Created on Febrero, 2019
+@author: arizbesilva0-star
+"""
+
+class Cuenta:
+
+    # Constructor
+    # Inicializa los atributos de la cuenta
+    def __init__(self, saldo, tipo, fechaCreacion):
+        self.saldo = saldo
+        self.tipo = tipo
+        self.fechaCreacion = fechaCreacion
+
+    # Método para depositar dinero
+    def depositar(self, cantidad):
+        self.saldo = self.saldo + cantidad
+
+    # Método para retirar dinero
+    def retirar(self, cantidad):
+        self.saldo = self.saldo - cantidad
+
+    # Método para mostrar la información de la cuenta
+    def imprimirDetalles(self):
+        print("Saldo:", self.saldo)
+        print("Tipo:", self.tipo)
+        print("Fecha de creación:", self.fechaCreacion)
